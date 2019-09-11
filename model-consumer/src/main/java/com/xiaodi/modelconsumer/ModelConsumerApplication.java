a1ae6fde50e2a0531fa9dc6093c1ca1401fcdadb
gongzhijian <545972892@qq.com>
2019-09-11 16:35:10 +0800

    Initial commit

---
+++ model-consumer/src/main/java/com/xiaodi/modelconsumer/ModelConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.xiaodi.modelconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelConsumerApplication.class, args);
+	}
+
+}
---
+++ model-provider/src/main/java/com/xiaodi/modelprovider/ModelProviderApplication.java
@@ -0,0 +1,13 @@
+package com.xiaodi.modelprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelProviderApplication.class, args);
+	}
+
+}
